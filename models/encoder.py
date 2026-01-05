f1a225e92ebcc3d46e0214ebbc3a2da84e944149
kyauk <96671721+kyauk@users.noreply.github.com>
2026-01-05 12:20:59 -0800

    fixed resnet bug

--- models/encoder.py
+++ models/encoder.py
@@ -10,9 +10,13 @@ from torchvision import models
 class ResNet50Encoder(nn.Module):
     def __init__(self):
         super(ResNet50Encoder,self).__init__()
-        self.model = models.resnet50(pretrained=True)
+        self.model = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
         # remove FC layer to get flattened embeddings
         self.model.fc = nn.Identity()
+        
+        # Freeze the backbone
+        for param in self.model.parameters():
+            param.requires_grad = False
        
 
     def forward(self, x):
